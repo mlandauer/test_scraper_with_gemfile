9436c25a009f1bec81cf80cbd3c42d82eb9181e3
Matthew Landauer <matthew@oaf.org.au>
2014-05-26 17:03:18 +1000

    Add example scraper with Gemfile and Procfile

--- scraper.rb
+++ scraper.rb
@@ -1,24 +1,66 @@
-# This is a template for a Ruby scraper on Morph (https://morph.io)
-# including some code snippets below that you should find helpful
+require 'scraperwiki'
+require 'mechanize'
+require 'logger'
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
-# # Read in a page
-# page = agent.get("http://foo.com")
-#
-# # Find somehing on the page using css selectors
-# p page.at('div.content')
-#
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
+starting_url = 'https://www2.bmcc.nsw.gov.au/datracking/Modules/applicationmaster/default.aspx?page=exhibit'
+comment_url = 'http://www.bmcc.nsw.gov.au/sustainableliving/developmentapplicationsinnotification'
 
-# You don't have to do things with the Mechanize or ScraperWiki libraries. You can use whatever gems are installed
-# on Morph for Ruby (https://github.com/openaustralia/morph-docker-ruby/blob/master/Gemfile) and all that matters
-# is that your final data is written to an Sqlite database called data.sqlite in the current working directory which
-# has at least a table called data.
+def clean_whitespace(a)
+  a.gsub("\r", ' ').gsub("\n", ' ').squeeze(" ").strip
+end
+
+def scrape_table(doc, comment_url)
+  doc.search('table tbody tr').each do |tr|
+    # Columns in table
+    # Show  Number  Exhibit Start  Exhibit End  Details  Village
+    tds = tr.search('td')
+    h = tds.map{|td| td.inner_html}
+
+    record = {
+      'info_url' => (doc.uri + tds[0].at('a')['href']).to_s,
+      'comment_url' => comment_url,
+      'council_reference' => clean_whitespace(h[1]),
+      'on_notice_from' => Date.strptime(clean_whitespace(h[2]),"%d/%m/%Y").to_s,
+      'on_notice_to' => Date.strptime(clean_whitespace(h[3]), "%d/%m/%Y").to_s,
+      'address' => clean_whitespace(h[4].split('<br>')[0]) + ", " + clean_whitespace(h[5]) + ", NSW",
+      'description' => clean_whitespace(h[4].split('<br>')[1..-1].join),
+      'date_scraped' => Date.today.to_s
+    }
+
+    #p record
+    # For simplicity sake for the time being always save the data (even if it's not a new application)
+    ScraperWiki.save_sqlite(['council_reference'], record)
+  end
+end
+
+def scrape_and_follow_next_link(doc, comment_url)
+  scrape_table(doc, comment_url)
+  nextButton = doc.at('.rgPageNext')
+  unless nextButton['onclick'] =~ /return false/
+    form = doc.forms.first
+
+    # The joy of dealing with ASP.NET
+    form['__EVENTTARGET'] = nextButton['name']
+    form['__EVENTARGUMENT'] = ''
+    # It doesn't seem to work without these stupid values being set.
+    # Would be good to figure out where precisely in the javascript these values are coming from.
+    form['ctl00%24RadScriptManager1']=
+      'ctl00%24cphContent%24ctl00%24ctl00%24cphContent%24ctl00%24Radajaxpanel2Panel%7Cctl00%24cphContent%24ctl00%24ctl00%24RadGrid1%24ctl00%24ctl03%24ctl01%24ctl10'
+    form['ctl00_RadScriptManager1_HiddenField']=
+      '%3B%3BSystem.Web.Extensions%2C%20Version%3D3.5.0.0%2C%20Culture%3Dneutral%2C%20PublicKeyToken%3D31bf3856ad364e35%3Aen-US%3A0d787d5c-3903-4814-ad72-296cea810318%3Aea597d4b%3Ab25378d2%3BTelerik.Web.UI%2C%20Version%3D2009.1.527.35%2C%20Culture%3Dneutral%2C%20PublicKeyToken%3D121fae78165ba3d4%3Aen-US%3A1e3fef00-f492-4ed8-96ce-6371bc241e1c%3A16e4e7cd%3Af7645509%3A24ee1bba%3Ae330518b%3A1e771326%3Ac8618e41%3A4cacbc31%3A8e6f0d33%3Aed16cbdc%3A58366029%3Aaa288e2d'
+    doc = form.submit(form.button_with(:name => nextButton['name']))
+    scrape_and_follow_next_link(doc, comment_url)
+  end
+end
+
+#ScraperWikiMorph.save_metadata('authority_name', 'Blue Mountains City Council')
+#ScraperWikiMorph.save_metadata('authority_short', 'Blue Mountains')
+#ScraperWikiMorph.save_metadata('state', 'NSW')
+
+# Using Mechanize to grab the page because ScraperWiki.scrape bombed out on me
+agent = Mechanize.new do |a|
+  a.verify_mode = OpenSSL::SSL::VERIFY_NONE
+end
+
+doc = agent.get(starting_url)
+scrape_and_follow_next_link(doc, comment_url)
